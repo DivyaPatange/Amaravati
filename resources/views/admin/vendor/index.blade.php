f6b1782f7115fa63a186a8dc5eebf921877200fd
divya <divyapatange0@gmail.com>
2021-03-11 18:11:16 +0530

    Initial Commit

---
+++ resources/views/admin/vendor/index.blade.php
@@ -0,0 +1,110 @@
+@extends('admin.admin_layout.main')
+@section('title', 'Vendor')
+@section('page_title', 'Vendor List')
+@section('customcss')
+<link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+@endsection
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        @if ($message = Session::get('success'))
+		<div class="alert alert-success alert-block mt-3">
+			<button type="button" class="close" data-dismiss="alert">×</button>	
+				<strong>{{ $message }}</strong>
+		</div>
+		@endif
+		@if ($message = Session::get('danger'))
+		<div class="alert alert-danger alert-block mt-3">
+			<button type="button" class="close" data-dismiss="alert">×</button>	
+				<strong>{{ $message }}</strong>
+		</div>
+		@endif
+    </div>
+</div>
+<div class="row mb-3">
+<div class="col-lg-12">
+    <div class="card mb-4">
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Vendor List</h6>
+        </div>
+        <div class="table-responsive p-3">
+            <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Business Owner Name</th>
+                        <th>Business Name</th>
+                        <th>Business Type</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Business Owner Name</th>
+                        <th>Business Name</th>
+                        <th>Business Type</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+<!--Row-->
+
+@endsection
+@section('customjs')
+<!-- Page level plugins -->
+<script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script>
+var SITEURL = '{{ route('admin.vendors.index')}}';
+$('#dataTableHover').DataTable({
+    processing: true,
+    serverSide: true,
+    ajax: {
+    url: SITEURL,
+    type: 'GET',
+    },
+    columns: [
+            { data: 'business_owner_name', name: 'business_owner_name' },
+            { data: 'business_name', name: 'business_name' },
+            { data: 'business_type', name: 'business_type' },
+            { data: 'username', name: 'username' },
+            { data: 'show_pwd', name: 'show_pwd' },
+            {data: 'action', name: 'action', orderable: false},
+        ],
+    order: [[0, 'desc']]
+});
+
+$('body').on('click', '#delete', function () {
+    var id = $(this).data("id");
+
+    if(confirm("Are You sure want to delete !")){
+        $.ajax({
+            type: "delete",
+            url: "{{ url('admin/vendors') }}"+'/'+id,
+            success: function (data) {
+            var oTable = $('#dataTableHover').dataTable(); 
+            oTable.fnDraw(false);
+            toastr.success(data.success);
+            },
+            error: function (data) {
+                console.log('Error:', data);
+            }
+        });
+    }
+});
+
+</script>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AdminLoginController;
+use App\Http\Controllers\Auth\AdminController;
+use App\Http\Controllers\Admin\VendorController;
+use App\Http\Controllers\Auth\VendorLoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('admin.login');
+});
+
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
+
+Route::get('/migrate', function () {
+    $exitCode = Artisan::call('migrate');
+    return 'DONE'; //Return anything
+});
+
+Route::get('/routeList', function () {
+    $exitCode = Artisan::call('route:list');
+    return Artisan::output(); //Return anything
+});
+
+Route::get('/seed', function () {
+    $exitCode = Artisan::call('db:seed');
+    return 'DONE'; //Return anything
+});
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    // Admin Authentication Route
+    Route::get('/login', [AdminLoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AdminLoginController::class, 'login'])->name('login.submit');
+    Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+    Route::get('/logout', [AdminLoginController::class, 'logout'])->name('logout');
+    Route::resource('/vendors', VendorController::class);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('vendors')->name('vendor.')->group(function() {
+    // Admin Authentication Route
+    Route::get('/login', [VendorLoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [VendorLoginController::class, 'login'])->name('login.submit');
+    Route::get('/', [App\Http\Controllers\Auth\VendorController::class, 'index'])->name('dashboard');
+    Route::get('/logout', [VendorLoginController::class, 'logout'])->name('logout');
+});
